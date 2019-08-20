4e5bea75047953578d20a42bfff5e66563df73a0
mjnathachandrappa@gmail.com <mjnathachandrappa@gmail.com>
2019-08-20 13:45:09 +0530

    Yatra Project commited

---
+++ VENV/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mr\projects1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Yatra/Yana/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class YanaConfig(AppConfig):
+    name = 'Yana'
